65b10318e6535555367bab7676613431f5a5f3d8
seankennelly <sean.kennelly@hotmail.co.uk>
2024-05-29 16:37:29 +0100

    Add Swagger documentation

--- server/server.js
+++ server/server.js
@@ -1,25 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 // SWAGGER
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerOptions = require('./Swagger/swaggerOptions');
+const swaggerOptions = require("./Swagger/swaggerOptions");
 const swaggerSpec = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
-
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 // MIDDLEWARE
 app.use(cors());
 
 //ROUTES
-const todoRouter = require('./src/routes/todo_routes');
-app.use('/todos', todoRouter);
+const todoRouter = require("./src/routes/todo_routes");
+app.use("/todos", todoRouter);
 
 // PORT
 const port = 5000;
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}`);
 });
-   
